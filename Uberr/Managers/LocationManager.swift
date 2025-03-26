6fcfc8decdc306ea6a7918c3598594513701d2bf
meetsuhagiya <meetsuhagiya420@gmail.com>
2025-03-26 16:29:13 +0530

    26/03/2025

--- Uberr/Managers/LocationManager.swift
+++ Uberr/Managers/LocationManager.swift
@@ -5,12 +5,21 @@
 //  Created by student on 25/03/25.
 //
 import CoreLocation
-class LocationManager: NSObject, CLLocationManagerDelegate {
+class LocationManager: NSObject, ObservableObject {
     private let locationManager = CLLocationManager()
     
     override init() {
+        super.init()
+        locationManager.delegate = self
         locationManager.desiredAccuracy = kCLLocationAccuracyBest
         locationManager.requestWhenInUseAuthorization()
         locationManager.startUpdatingLocation( )
+    }
+}
+
+extension LocationManager: CLLocationManagerDelegate{
+ func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+     guard !locations.isEmpty else { return }
+     locationManager .stopUpdatingLocation()
     }
 }
